1458f17ae2fdcac18d1715cf87fa7be6521f6a54
Roberto Camargo <betocf2511@gmail.com>
2024-08-20 21:35:23 -0300

    nome da solução alterado

--- Domain/Models/ViewModel/UsuarioAPIViewModel.cs
+++ Domain/Models/ViewModel/UsuarioAPIViewModel.cs
@@ -8,13 +8,13 @@ namespace Usuario_API.Models
         [Required(ErrorMessage = "Inclua o Id do Usuário")]
         public int Id { get; set; }
         [Required(ErrorMessage = "Inclua o Nome do Usuário")]
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         [Required(ErrorMessage = "Inclua o Sobrenome do Usuário")]
-        public string Sobrenome { get; set; }
+        public string? Sobrenome { get; set; }
         [Required(ErrorMessage = "Inclua a Situação do Usuário")]
         public bool Ativo { get; set; }
-        public string Departamento { get; set; }
-        public string Turno { get; set; }
+        public string? Departamento { get; set; }
+        public string? Turno { get; set; }
         public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
         public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
     }
